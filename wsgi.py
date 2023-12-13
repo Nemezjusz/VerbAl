1e95bf247116bc349f67df25e1f65066065772c1
Szymon Sadowski <50834734+Nemezjusz@users.noreply.github.com>
2023-12-13 12:14:52 +0100

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os
-import werkzeug
+from flask import Flask, render_template, request, redirect
+import os, werkzeug, whisper
+from openai import OpenAI
+
+client = OpenAI(api_key="api-key")
 
 app = Flask(__name__)
 
@@ -8,6 +10,23 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'  # make sure this directory exists
 app.config['ALLOWED_EXTENSIONS'] = {'wav', 'mp3', 'aac', 'ogg', 'm4a'}
 
+model = whisper.load_model("base")
+
+
+def transcribe_audio(file_path):
+    result = model.transcribe(file_path)
+    return result['text']
+
+
+def correct_grammer(transcript):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a grammar assistant, skilled in correcting grammar"},
+            {"role": "user", "content": f"Correct grammer in following text, return only corrected text: {transcript}"}
+        ]
+    )
+    return completion.choices[0].message.content
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
@@ -34,13 +53,14 @@ def upload_file():
         filename = werkzeug.utils.secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
-        # Process the file if needed
-        # ...
 
-        # Redirect or render a template after saving
-        return redirect(url_for('index'))
+        text = transcribe_audio(filepath)
+        corrected_text = correct_grammer(text)
+
+        return render_template("main.html", transcript=text, corrected_transcript=corrected_text)
 
     return redirect(request.url)
+
 
 
 if __name__ == '__main__':
