d6bf892e6cbdacb9e49820fc8aba8fb7248cc224
Szymon Sadowski <50834734+Nemezjusz@users.noreply.github.com>
2023-12-17 17:07:19 +0100

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect
-import os, werkzeug, whisper
+import os, werkzeug, whisper, difflib
 from openai import OpenAI
 
-client = OpenAI(api_key="api-key")
+client = OpenAI(api_key="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")
 
 app = Flask(__name__)
 
@@ -23,20 +23,30 @@ def correct_grammer(transcript):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a grammar assistant, skilled in correcting grammar"},
-            {"role": "user", "content": f"Correct grammar in following text, return only corrected text: {transcript}"}
+            {"role": "user", "content": f"Correct grammer in following text, return only corrected text: {transcript}"}
         ]
     )
     return completion.choices[0].message.content
 
 
-def find_changed_words(original_text, corrected_text):
-    original_words = original_text.split()
-    corrected_words = corrected_text.split()
+def find_different_words(original_text, corrected_text):
+    d = difflib.Differ()
+    diff = list(d.compare(corrected_text.split(), original_text.split() ))
 
-    changed_words = [word for original, corrected in zip(original_words, corrected_words) if original != corrected]
+    different_words = [word[2:] for word in diff if word.startswith('- ')]
 
-    return changed_words
+    return different_words
+def replace_with_bold(corrected_text, changed_words):
+    corrected_text = corrected_text.split()
+    c = 0
+    for i in range(len(corrected_text)):
+        if corrected_text[i] == changed_words[c]:
+            corrected_text[i] = f"<b style='color:red;'>{corrected_text[i]}<b/>"
+            if c != len(changed_words)-1:
+                c+=1
 
+    result_text = ' '.join(corrected_text)
+    return result_text
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
@@ -44,7 +54,10 @@ def allowed_file(filename):
 
 @app.route('/')
 def index():
-    return render_template("main.html")
+    text = "After loading your file using Audio File button or providing it via Microphone you will see " \
+           "your transcribed text in this section"
+    corrected_text = "Here you will see <b>corrected</b> text"
+    return render_template("main.html", transcript=text, corrected_transcript=corrected_text)
 
 
 @app.route('/upload', methods=['POST'])
@@ -59,6 +72,7 @@ def upload_file():
     if file.filename == '':
         return redirect(request.url)
 
+
     if file and allowed_file(file.filename):
         filename = werkzeug.utils.secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
@@ -66,8 +80,10 @@ def upload_file():
 
         text = transcribe_audio(filepath)
         corrected_text = correct_grammer(text)
+        changed_words = find_different_words(text, corrected_text)
+        corrected_and_edited_text = replace_with_bold(corrected_text, changed_words)
 
-        return render_template("main.html", transcript=text, corrected_transcript=corrected_text)
+        return render_template("main.html", transcript=text, corrected_transcript=corrected_and_edited_text)
 
     return redirect(request.url)
 
